8afcaf8022dffb0ae1272cbb3b884e523b77e0c2
Fallopio <kibaryshnikov@yandex.ru>
2020-04-04 06:13:21 +0400

    init

--- src/containers/List/AnimeList.jsx
+++ src/containers/List/AnimeList.jsx
@@ -13,26 +13,8 @@ function AnimeList(props) {
                 <div key={item.id} id={item.id}>
                     <h3 className='cardH'>
                         {item.attributes.canonicalTitle}
-                    </h3>
-                    <p>
-                        Avrage Rating:   {item.attributes.averageRating}
-                    </p>
-                    <p>
-                        Popularity rank: {item.attributes.popularityRank}
-                    </p>
-                    <p>
-                        Age rating: {item.attributes.ageRating}
-                    </p>
-                    <p>
-                        Number of episodes: {item.attributes.episodeCount}
-                    </p>
-                    <p>
-                        Status: {item.attributes.status}
-                    </p>
-                    <img className='img' src={item.attributes.posterImage.medium} alt="" />
-                    <p className='descHide' id={item.id + 'desc'}>
-                        Synopsis: {item.attributes.synopsis}
-                    </p>
+                    </h3>                   
+                    <img className='img' src={item.attributes.posterImage.medium} alt="" />                    
                 </div>
             )}
             <button onClick={
