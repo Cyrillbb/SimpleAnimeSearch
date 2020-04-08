29048548c9a5815c9acbac1aed40e63fb59d803d
Fallopio <kibaryshnikov@yandex.ru>
2020-04-08 21:31:24 +0400

    Updates

--- precache-manifest.57533a0c9e9b609c2f93143d8615c612.js
+++ precache-manifest.57533a0c9e9b609c2f93143d8615c612.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d2531b2461039b4b68bdda739305e0b9",
+    "revision": "5e075247fb759e1975f5bffb52d75a38",
     "url": "/SimpleAnimeSearch/index.html"
   },
   {
-    "revision": "3374a70a145adc9c94aa",
+    "revision": "a85aa2a10924da022995",
     "url": "/SimpleAnimeSearch/static/css/main.8df52c7e.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/SimpleAnimeSearch/static/js/2.2767fa97.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3374a70a145adc9c94aa",
-    "url": "/SimpleAnimeSearch/static/js/main.2a535160.chunk.js"
+    "revision": "a85aa2a10924da022995",
+    "url": "/SimpleAnimeSearch/static/js/main.509a901d.chunk.js"
   },
   {
     "revision": "f1d00eac611a35daa248",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SimpleAnimeSearch/precache-manifest.869001d776e3b89677d1b2e23f23af20.js"
+  "/SimpleAnimeSearch/precache-manifest.57533a0c9e9b609c2f93143d8615c612.js"
 );
 
 self.addEventListener('message', (event) => {
