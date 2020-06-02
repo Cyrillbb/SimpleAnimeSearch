c1e7d487b1e702c4f6b93c4ae6cfe5ef90d44c71
Fallopio <kibaryshnikov@yandex.ru>
2020-06-02 22:52:18 +0400

    Updates

---
+++ precache-manifest.7edfd4cb9064181829fc8fb721b06e00.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa557bd4d6ab422bbf2edc55b4576cde",
+    "url": "/SimpleAnimeSearch/index.html"
+  },
+  {
+    "revision": "a237f2c4bc4efa2a93de",
+    "url": "/SimpleAnimeSearch/static/css/main.6ca4cf5c.chunk.css"
+  },
+  {
+    "revision": "60bacf346bbc9e836794",
+    "url": "/SimpleAnimeSearch/static/js/2.2b09b68a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SimpleAnimeSearch/static/js/2.2b09b68a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a237f2c4bc4efa2a93de",
+    "url": "/SimpleAnimeSearch/static/js/main.ef893bdb.chunk.js"
+  },
+  {
+    "revision": "f0b7c0efd83b03ca0503",
+    "url": "/SimpleAnimeSearch/static/js/runtime-main.6714c055.js"
+  }
+]);
